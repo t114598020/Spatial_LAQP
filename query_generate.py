e10eb5634932201a55425ce3e7192ca200c3b622
t114598020 <t114598020@ntut.org.tw>
2025-12-29 00:02:38 +0800

    Add files via upload

--- query_generate.py
+++ query_generate.py
@@ -47,16 +47,20 @@ def generate_random_query(data, dimensions, test=False):
     return predicates
 
 def generate_uber_query_log(num_queries, data, sample, dimensions, full_data_size):
+    attempts = 0
+    max_attempts = 10000
+
     print("Generating uber query log...")
     query_log = []
-    while len(query_log) < num_queries:
+    while len(query_log) < num_queries and attempts < max_attempts:
         q = generate_bounded_random_query(data, dimensions)
         exact = exact_count(q, data)
         estimate = sample_count(q, sample, full_data_size)
-        if exact > 100 and estimate > 0:  # Skip zeros
+        if exact > 100 and estimate > 0:  # make sure valuable
             error = exact - estimate
             query_log.append({'query': q, 'exact': exact, 'estimate': estimate, 'error': error})
-    print(f"Generated {len(query_log)} queries")
+        attempts+=1
+    print(f"Generated {len(query_log)} queries in {attempts} attempts")
     return query_log
 
 def generate_power_query_log(agg_col, num_queries, data, sample, dimensions, full_data_size, avg_exact=0):
@@ -69,13 +73,13 @@ def generate_power_query_log(agg_col, num_queries, data, sample, dimensions, ful
         threshold = 0.01 * avg_exact
 
     print("Generating power query log...")
+    # print("The threshold is ", threshold)
     query_log = []
     while len(query_log) < num_queries and attempts < max_attempts:
         q = generate_bounded_random_query(data, dimensions)
         exact_result = exact_sum(agg_col, q, data)
-
-        if exact_result > threshold:  # the result will propbably be 0, make threshold to 1 to ensure valuable result.
-            estimate = sample_sum(agg_col, q, sample, full_data_size)
+        estimate = sample_sum(agg_col, q, sample, full_data_size)
+        if exact_result > threshold and estimate > 0:  # the result will propbably be 0, make threshold to 1 to ensure valuable result.
             error = exact_result - estimate
             query_log.append({'query': q, 'exact': exact_result, 
                             'estimate': estimate, 'error': error})
@@ -84,8 +88,8 @@ def generate_power_query_log(agg_col, num_queries, data, sample, dimensions, ful
     if avg_exact==0 :
         avg_exact = np.mean([temp_query['exact'] for temp_query in query_log])
         print(f"Average exact sum: {avg_exact=}")
-        print(f"Generated {len(query_log)} queries\n")
+        print(f"Generated {len(query_log)} queries in {attempts} attempts\n")
         return avg_exact, query_log
     else:
-        print(f"Generated {len(query_log)} queries\n")
+        print(f"Generated {len(query_log)} queries in {attempts} attempts\n")
         return query_log
